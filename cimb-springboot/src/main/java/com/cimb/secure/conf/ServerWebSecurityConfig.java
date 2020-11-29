81d7303630d9def93e6eba4272cd272b99dd9b1f
Tu Phan <tuantu.phan@cimb.com>
2020-11-29 12:37:41 +0800

    Update Security

--- cimb-springboot/src/main/java/com/cimb/secure/conf/ServerWebSecurityConfig.java
+++ cimb-springboot/src/main/java/com/cimb/secure/conf/ServerWebSecurityConfig.java
@@ -1,18 +1,30 @@
 package com.cimb.secure.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class ServerWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    @Autowired
+    private DataSource dataSource;
+
     @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable().authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers(HttpMethod.POST, "/login").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore()
     }
 }
